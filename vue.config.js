8a0156de68aa64ca1a42544e12c97647f0c9663e
devgiordane <bruno_giordane@outlook.com>
2020-09-08 21:46:42 -0300

    Versão 1.0 - prerender-spa-plugin

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,24 @@
-// Inside vue.config.js
+const path = require("path");
+const PrerenderSPAPlugin = require("prerender-spa-plugin");
+
 module.exports = {
   // ...other vue-cli plugin options...
   pwa: {
     workboxOptions: {
       exclude: ["_redirects"]
     }
+  },
+  configureWebpack: {
+    plugins: [
+      new PrerenderSPAPlugin({
+        staticDir: path.join(__dirname, "dist"),
+        routes: [
+          "/",
+          "/about",
+          "/gerador-whatsapp",
+          "/gerador-de-border-radius"
+        ]
+      })
+    ]
   }
 };
